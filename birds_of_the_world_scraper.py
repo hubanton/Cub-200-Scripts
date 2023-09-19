1f3dd5129bd4977c542d657894fd2abd5a4ab875
hubanton <anton.huber98@yahoo.com>
2023-09-19 21:38:31 +0200

    feat(webscraper): add try-catch for missing birds

--- birds_of_the_world_scraper.py
+++ birds_of_the_world_scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 import pyautogui
 import os
@@ -13,35 +14,45 @@ load_dotenv()
 
 filepath = 'birds_of_the_world_files'
 
-birdnames = ['Black-footed Albatross']
+birdnames = ['asdasddasdsadsadsa', 'Black-footed Albatross']
+
+not_found_birds = []
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
+driver.get("https://birdsoftheworld.org/bow/home")
+
+login = driver.find_element(By.LINK_TEXT, 'Sign In')
+
+login.click()
+WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
+
+username = driver.find_element(By.ID, 'input-user-name')
+username.send_keys(os.getenv('BENUTZER'))
+password = driver.find_element(By.ID, 'input-password')
+password.send_keys(os.getenv('PASSWORT'))
+
+confirm_button = driver.find_element(By.ID, 'form-submit')
+confirm_button.click()
+
+WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
+
 for bird in birdnames:
     driver.get("https://birdsoftheworld.org/bow/home")
-
-    login = driver.find_element(By.LINK_TEXT, 'Sign In')
-
-    login.click()
-    WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
-
-    username = driver.find_element(By.ID, 'input-user-name')
-    username.send_keys(os.getenv('BENUTZER'))
-    password = driver.find_element(By.ID, 'input-password')
-    password.send_keys(os.getenv('PASSWORT'))
-
-    confirm_button = driver.find_element(By.ID, 'form-submit')
-    confirm_button.click()
-
     WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
 
     inputField = driver.find_element(By.ID, "hero")
     inputField.send_keys(bird)
 
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Suggest-suggestion-0")))
+    try:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Suggest-suggestion-0")))
+    except TimeoutException:
+        print(f'Could not find bird: {bird}')
+        not_found_birds.append(bird)
+        continue
 
     element.click()
 
@@ -62,3 +73,5 @@ for bird in birdnames:
     pyautogui.typewrite(bird)
     pyautogui.hotkey('enter')
     time.sleep(5)
+
+print(not_found_birds)
