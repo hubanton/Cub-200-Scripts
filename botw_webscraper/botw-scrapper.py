7112cfd3c932a7c6f42e9702ec303d453594f7bd
hubanton <anton.huber98@yahoo.com>
2023-09-21 13:37:22 +0200

    feat(general): improve botw-scrapper.py

--- botw_webscraper/botw-scrapper.py
+++ botw_webscraper/botw-scrapper.py
@@ -11,10 +11,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+link_text = 'Sounds and Vocal Behavior'
+
 english_names_file = '../shared/en_names.txt'
 bird_names_file = '../shared/botw-names.txt'
 
+textfiles = 'textfiles'
+
+if not os.path.exists(textfiles):
+    os.mkdir(textfiles)
+
 not_found_names = 'not-found-names.txt'
+
 
 
 def load_names(path):
@@ -22,67 +30,84 @@ def load_names(path):
         return f.read().splitlines()
 
 
-bird_names = load_names(bird_names_file)
-english_names_file = load_names(english_names_file)
+birds = load_names(bird_names_file)
+en_birds = load_names(english_names_file)
 
-not_found_birds = []
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+def scrape_botw(bird_names, english_names, download_pdf=True, download_section=True):
+    not_found_birds = []
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://birdsoftheworld.org/bow/home")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
 
-login = driver.find_element(By.LINK_TEXT, 'Sign In')
-login.click()
-
-WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
-
-username = driver.find_element(By.ID, 'input-user-name')
-username.send_keys(os.getenv('BENUTZER'))
-
-password = driver.find_element(By.ID, 'input-password')
-password.send_keys(os.getenv('PASSWORT'))
-
-confirm_button = driver.find_element(By.ID, 'form-submit')
-confirm_button.click()
-
-WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
-
-for bird_name, english_name in zip(bird_names, english_names_file):
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://birdsoftheworld.org/bow/home")
-    WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
 
-    inputField = driver.find_element(By.ID, "hero")
-    inputField.send_keys(bird_name)
-
-    try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Suggest-suggestion-0")))
-    except TimeoutException:
-        print(f'Could not find bird: {bird_name}')
-        not_found_birds.append(bird_name)
-        continue
-
-    element.click()
+    login = driver.find_element(By.LINK_TEXT, 'Sign In')
+    login.click()
 
     WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
 
-    category = driver.find_element(By.LINK_TEXT, 'Sounds and Vocal Behavior')
-    category.click()
+    username = driver.find_element(By.ID, 'input-user-name')
+    username.send_keys(os.getenv('BENUTZER'))
+
+    password = driver.find_element(By.ID, 'input-password')
+    password.send_keys(os.getenv('PASSWORT'))
+
+    confirm_button = driver.find_element(By.ID, 'form-submit')
+    confirm_button.click()
+
     WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
 
-    # sections = driver.find_elements(By.TAG_NAME, 'p')
-    # introduction = sections[0].text
+    for bird_name, english_name in zip(bird_names, english_names):
+        driver.get("https://birdsoftheworld.org/bow/home")
+        WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
 
-    # Download as pdf
-    pyautogui.hotkey('ctrl', 'p')
-    time.sleep(5)
-    pyautogui.hotkey('enter')
-    time.sleep(2)
-    pyautogui.typewrite(english_name)
-    pyautogui.hotkey('enter')
-    time.sleep(5)
+        inputField = driver.find_element(By.ID, "hero")
+        inputField.send_keys(bird_name)
 
-with open(not_found_names, 'w') as missing_birds:
-    missing_list = '\n'.join(not_found_birds)
-    missing_birds.write(missing_list)
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Suggest-suggestion-0")))
+        except TimeoutException:
+            print(f'Could not find bird: {bird_name}')
+            not_found_birds.append(bird_name)
+            continue
+
+        element.click()
+
+        WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
+
+        category = driver.find_element(By.LINK_TEXT, link_text)
+        category.click()
+        WebDriverWait(driver, 10).until(EC.url_changes(driver.title))
+
+        if download_section:
+            sections = driver.find_elements(By.CLASS_NAME, 'GridFlex-cell')
+            with open(textfiles + '/' + english_name + '.txt', 'w') as bird_info:
+                text = sections[3].text
+                if not text.startswith(link_text):
+                    text = text.split(link_text)[1]
+                    text = 'Sounds and Vocal Behavior' + text
+
+                text = text.split('Diet and Foraging')[0]
+                if len(text.split('Breeding')) != 1:
+                    text = text.split('Breeding')[0]
+
+                bird_info.write(text)
+
+        if download_pdf:
+            # Download as pdf
+            pyautogui.hotkey('ctrl', 'p')
+            time.sleep(5)
+            pyautogui.hotkey('enter')
+            time.sleep(2)
+            pyautogui.typewrite(english_name)
+            pyautogui.hotkey('enter')
+            time.sleep(5)
+
+    with open(not_found_names, 'w') as missing_birds:
+        missing_list = '\n'.join(not_found_birds)
+        missing_birds.write(missing_list)
+
+
+scrape_botw(birds, en_birds, download_pdf=False, download_section=True)
