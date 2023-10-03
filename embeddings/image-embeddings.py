b871fbee8084607415e1257213392a435b16aabe
hubanton <anton.huber98@yahoo.com>
2023-10-03 14:41:06 +0200

    feat(general): Extend / Improve embedding

--- embeddings/image-embeddings.py
+++ embeddings/image-embeddings.py
@@ -1,8 +1,22 @@
-from transformers import ViTImageProcessor, ViTModel
+from transformers import AutoImageProcessor, AutoModel
+from tqdm.contrib import tzip
 from PIL import Image
+import tensorrt
 import pandas as pd
 import numpy as np
 import os
+import torch
+import transformers
+
+transformers.logging.set_verbosity_error()
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+cub_200_names_file = '../shared/cub-200-folder-names.txt'
+latin_names_file = '../shared/latin_names.txt'
+root_directory = '../CUB_200_2011/images'
+
+used_models = ['google/vit-base-patch16-224-in21k', 'microsoft/resnet-34', 'microsoft/focalnet-tiny']
 
 
 def readfile(path):
@@ -21,32 +35,31 @@ def store_as_csv(embedding_dict, path):
         new_column_names[i] = f'Neuron {i + 1}'
     df.rename(columns=new_column_names, inplace=True)
 
-    # Save the DataFrame to a CSV file
     df.to_csv(path, index=False)
 
 
-def calculate_class_mean_embeddings(model, root_dir, folder_names, save_names, normalize=False):
-    processor = ViTImageProcessor.from_pretrained(model_name)
-    model = ViTModel.from_pretrained(model)
+def get_image_embeddings(image, model, preprocessor):
+    image = preprocessor(images=image, return_tensors="pt").to(device)
+    outputs = model(**image)
 
+    return outputs.pooler_output.squeeze().detach().cpu().numpy()
+
+
+def get_mean_embeddings(model, preprocessor, root_dir, folder_names, save_names, normalize=False):
     mean_embeddings = {}
 
-    for folder, save_name in zip(folder_names, save_names):
+    for folder, save_name in tzip(folder_names, save_names):
         subdirectory = root_dir + '/' + folder
-
-        class_files = os.listdir(root_dir + '/' + folder)
-
-        print(len(class_files), folder, save_name)
+        class_files = os.listdir(subdirectory)
 
         class_embeddings = []
 
         for img_path in class_files:
             file_path = os.path.join(subdirectory, img_path)
-            image = Image.open(file_path)
-            image = processor(images=image, return_tensors="pt")
+            image = Image.open(file_path).convert('RGB')
 
-            outputs = model(**image)
-            embedding = outputs.last_hidden_state.mean(dim=1).squeeze().detach().numpy()
+            embedding = get_image_embeddings(image, model, preprocessor)
+
             class_embeddings.append(embedding)
 
         mean_embedding = sum(class_embeddings) / len(class_embeddings)
@@ -59,14 +72,12 @@ def calculate_class_mean_embeddings(model, root_dir, folder_names, save_names, n
     return mean_embeddings
 
 
-model_name = 'google/vit-base-patch16-224-in21k'
-root_directory = '../CUB_200_2011/images'
-cub_200_names_file = '../shared/cub-200-folder-names.txt'
-latin_names_file = '../shared/latin_names.txt'
+cub_200_names = readfile(cub_200_names_file)
+latin_names = readfile(latin_names_file)
 
-cub_200_names = readfile(cub_200_names_file)[:3]
-latin_names = readfile(latin_names_file)[:3]
+for model_name in used_models:
+    proc = AutoImageProcessor.from_pretrained(model_name)
+    mod = AutoModel.from_pretrained(model_name).to(device)
 
-class_representations = calculate_class_mean_embeddings(model_name, root_directory, cub_200_names, latin_names)
-
-store_as_csv(class_representations, 'class_embeddings.csv')
+    class_reps = get_mean_embeddings(mod, proc, root_directory, cub_200_names, latin_names)
+    store_as_csv(class_reps, f'class_embeddings [{model_name.replace("/", "_")}].csv')
