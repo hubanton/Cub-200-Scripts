45bf730105a2ce67b86336289173a817fee83ca6
hubanton <anton.huber98@yahoo.com>
2023-11-16 15:03:18 +0100

    feat(general): add first working version of clap_eval

--- helper_scripts/evaluate_clap.py
+++ helper_scripts/evaluate_clap.py
@@ -1,30 +1,33 @@
 import os
 
+import audiofile
 import pandas as pd
 import torch
-import torchaudio
-from sklearn.metrics import f1_score, accuracy_score, recall_score
-from transformers import AutoProcessor, ClapModel
+from tqdm import tqdm
+from transformers import ClapModel, RobertaTokenizerFast, ClapFeatureExtractor
 
 audio_directory = '../xeno_canto_data/Recordings'
 text_directory = '../botw_data/Sounds_and_Vocal_Behavior/v3'
 cub_200_directory = './cub-200'
-splits = [str(i) for i in range(5)]
 
-model = ClapModel.from_pretrained("laion/clap-htsat-unfused")
-processor = AutoProcessor.from_pretrained("laion/clap-htsat-unfused")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+model = ClapModel.from_pretrained("laion/clap-htsat-unfused").to(device)
+
+text_encoder = RobertaTokenizerFast.from_pretrained("laion/clap-htsat-unfused")
+audio_extractor = ClapFeatureExtractor.from_pretrained("laion/clap-htsat-unfused")
 
 
 # Function to load audio samples from a directory
-def load_audio_from_directory(directory):
-    audio_samples = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".mp3"):
-                file_path = os.path.join(root, file)
-                waveform, _ = torchaudio.load(file_path)
-                audio_samples.append(waveform)
-    return audio_samples
+def load_audio_from_path(path):
+    waveform = audiofile.read(path, always_2d=True)[0]
+    waveform = waveform.mean(0, keepdims=True)
+    return waveform
+
+
+def write_list_to_csv(file_path, data):
+    df = pd.DataFrame(data, columns=["Species"])
+    df.to_csv(file_path, index=False)
 
 
 def load_text_from_directory(directory):
@@ -33,13 +36,11 @@ def load_text_from_directory(directory):
     return text
 
 
-# Initialize lists to store evaluation metrics for each split
-f1_scores = []
-accuracy_scores = []
-uar_scores = []
+for split in range(1, 5):
+    predictions = []
+    labels = []
 
-for split in splits:
-    split_directory = os.path.join(cub_200_directory, split)
+    split_directory = os.path.join(cub_200_directory, str(split))
     test_csv_path = os.path.join(split_directory, 'test.csv')
 
     labels_in_split = pd.read_csv(test_csv_path)['species'].tolist()
@@ -49,43 +50,38 @@ for split in splits:
                          labels_in_split]
 
     # Iterate over species folders in the split
-    for index, species_folder in enumerate(labels_in_split):
-        text_folder_path = os.path.join(audio_directory, species_folder)
-        # Load text descriptions for the current species
-        audio_sample = load_audio_from_directory(text_folder_path)
+    for index, species_folder in enumerate(tqdm(labels_in_split)):
+        bird_folder_path = os.path.join(audio_directory, species_folder)
 
-        print(text_descriptions)
-        print(len(text_descriptions))
-        print(len(labels_in_split))
-        inputs = processor(text=text_descriptions, audios=audio_sample, return_tensors="pt", padding=True)
+        birds_from_species = os.listdir(bird_folder_path)
 
-        outputs = model(**inputs)
-        logits_per_audio = outputs.logits_per_audio
-        probs = logits_per_audio.softmax(dim=-1)
+        for bird in tqdm(birds_from_species):
+            bird_path = os.path.join(bird_folder_path, bird)
 
-        # Assuming you have ground truth labels for each test sample
-        ground_truth_labels = [species_folder] * len(audio_sample)
+            audio_sample = load_audio_from_path(bird_path)
 
-        # Convert probabilities to predicted labels
-        predicted_labels = torch.argmax(probs, dim=-1).tolist()
+            with torch.no_grad():
+                # Note that we could also use ClapProcessor which wraps the tokenizer and the feature extractor
+                # However setting truncation throws an error (present on both parts) so we do it manually.
 
-        # Calculate F1 score
-        f1 = f1_score(ground_truth_labels, predicted_labels, average='weighted')
-        f1_scores.append(f1)
+                encoding = text_encoder(text_descriptions, padding=True, max_length=512, truncation=True,
+                                        return_tensors="pt")
 
-        # Calculate accuracy
-        accuracy = accuracy_score(ground_truth_labels, predicted_labels)
-        accuracy_scores.append(accuracy)
+                encoding["input_features"] = audio_extractor(audio_sample, sampling_rate=48000,
+                                                             return_tensors="pt").input_features
 
-        # Calculate Unweighted Average Recall (UAR)
-        uar = recall_score(ground_truth_labels, predicted_labels, average='macro')
-        uar_scores.append(uar)
+                encoding.to(device)
 
-# Calculate the average scores across all splits
-avg_f1 = sum(f1_scores) / len(f1_scores)
-avg_accuracy = sum(accuracy_scores) / len(accuracy_scores)
-avg_uar = sum(uar_scores) / len(uar_scores)
+                outputs = model(**encoding)
+                logits_per_audio = outputs.logits_per_audio
+                probs = logits_per_audio.softmax(dim=-1).squeeze()
 
-print(f"Average F1 Score: {avg_f1}")
-print(f"Average Accuracy: {avg_accuracy}")
-print(f"Average UAR: {avg_uar}")
+            ground_truth = index
+
+            predicted_label = torch.argmax(probs).cpu().item()
+
+            labels.append(ground_truth)
+            predictions.append(predicted_label)
+
+    write_list_to_csv(f'{split}_predictions', predictions)
+    write_list_to_csv(f'{split}_ground_truths', labels)
